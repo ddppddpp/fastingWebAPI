cf85cfd99f1e2bb881e6c9c590bc77237b6da778
ddppddpp <djidji.perroto@gmail.com>
2021-12-27 09:50:00 +0200

    added Weekly and Monthly paths and responses

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # api that accepts a date as a query param and returns an int (0..6) as resposne
-from typing import Optional
-from datetime import date, datetime
+from typing import Optional, List
+from datetime import date, datetime, timedelta
 
 import pydantic
+import calendar
 from context import bgchof
 from bgchof import getStatusForDate
 from fastapi import FastAPI, Query
@@ -14,10 +15,19 @@ class dateStatus(BaseModel):
     status: conint(ge=0, le=6)
 
 
+def firstDayOfWeekforDate(inputDate: date):
+    dayOfWeek = inputDate.weekday()
+    weeknumber = inputDate.isocalendar()[1]
+    resultFirstDayOfWeek = datetime.strptime(
+        f"{inputDate.year}-{weeknumber}-1", "%Y-%W-%w"
+    ).date()
+    return resultFirstDayOfWeek
+
+
 app = FastAPI()
 
 
-@app.get("/fastingStatus", response_model=dateStatus)
+@app.get("/fastingStatus/date", response_model=dateStatus)
 async def read_items(
     inputDate: Optional[date] = Query(
         None,
@@ -28,6 +38,50 @@ async def read_items(
         theDate = inputDate
     else:
         theDate = date.today()
-        result = {"the_date": theDate, "status": bgchof.getStatusForDate(theDate)}
 
+    result = {"the_date": theDate, "status": bgchof.getStatusForDate(theDate)}
+    return result
+
+
+# week of dates
+@app.get("/fastingStatus/week", response_model=List[dateStatus])
+async def read_items(
+    inputDate: Optional[date] = Query(
+        None,
+        description="Date in YYYY-MM-DD format for which to calculate the Fasting Status. If Empty defaults to today.",
+    )
+):
+    if inputDate:
+        theDate = inputDate
+    else:
+        theDate = date.today()
+    mondayForThisWeek = firstDayOfWeekforDate(theDate)
+
+    result = list[dateStatus]()
+    for i in range(0, 7):
+        tempDate = mondayForThisWeek + timedelta(days=i)
+        result.append(
+            {"the_date": tempDate, "status": bgchof.getStatusForDate(tempDate)}
+        )
+    return result
+
+
+# month of dates
+@app.get("/fastingStatus/month", response_model=List[dateStatus])
+async def read_items(
+    inputDate: Optional[date] = Query(
+        None,
+        description="Date in YYYY-MM-DD format for which to calculate the Fasting Status. If Empty defaults to today.",
+    )
+):
+    if inputDate:
+        theDate = inputDate
+    else:
+        theDate = date.today()
+    result = list[dateStatus]()
+    for i in range(1, calendar.monthrange(theDate.year, theDate.month)[1] + 1):
+        tempDate = date(theDate.year, theDate.month, i)
+        result.append(
+            {"the_date": tempDate, "status": bgchof.getStatusForDate(tempDate)}
+        )
     return result
